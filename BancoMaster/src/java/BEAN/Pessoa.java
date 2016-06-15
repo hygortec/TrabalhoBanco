7a2c3706fa04e9164abc1486f315ea5b35081e98
higor <higor@DESKTOP-BF297VI.mshome.net>
2016-06-15 20:12:38 -0300

    Criei a classe Pessoa

---
+++ BancoMaster/src/java/BEAN/Pessoa.java
@@ -0,0 +1,90 @@
+package BEAN;
+
+/**
+ *
+ * @author higor
+ */
+public class Pessoa {
+    private int id;
+    private String nome;
+    private String endereco;
+    private String cpf;
+    private String rg;
+    private String telefone;
+    private int sexo;
+    private int tipoPessoa;
+    private float rendaMensal;
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public int getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(int sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getTipoPessoa() {
+        return tipoPessoa;
+    }
+
+    public void setTipoPessoa(int tipoPessoa) {
+        this.tipoPessoa = tipoPessoa;
+    }
+
+    public float getRendaMensal() {
+        return rendaMensal;
+    }
+
+    public void setRendaMensal(float rendaMensal) {
+        this.rendaMensal = rendaMensal;
+    }
+    
+}
