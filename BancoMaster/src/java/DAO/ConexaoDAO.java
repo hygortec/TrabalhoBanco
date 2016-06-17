71816920a6055faead3f0728753833dc46054ece
higor <higor@DESKTOP-BF297VI.mshome.net>
2016-06-17 20:42:42 -0300

    Correções diversas

--- BancoMaster/src/java/DAO/ConexaoDAO.java
+++ BancoMaster/src/java/DAO/ConexaoDAO.java
@@ -18,21 +18,19 @@ public class ConexaoDAO {
          
         try{
             Class.forName("com.mysql.jdbc.Driver");
-            Connection conm = DriverManager.getConnection("jdbc:mysql://db4free.net","bancomaster","shaman");
+            //Connection conm = DriverManager.getConnection("jdbc:mysql://db4free.net","bancomaster","shaman");
+            Connection conm = DriverManager.getConnection("jdbc:mysql://localhost","root","cronos");
             String sql = "use bancomaster";
 
             PreparedStatement stmt = conm.prepareStatement(sql);
             stmt.close();
             conm.close();
             
-            return DriverManager.getConnection("jdbc:mysql://db4free.net/bancomaster","bancomaster","shaman");
+            //return DriverManager.getConnection("jdbc:mysql://db4free.net/bancomaster","bancomaster","shaman");
+            return DriverManager.getConnection("jdbc:mysql://localhost/bancomaster","root","cronos");
         }catch(ClassNotFoundException ex){
             throw new SQLDataException(ex.getMessage());
         }
-        
-        
-        
-        
     }
     
 }
---
+++ BancoMaster/src/java/DAO/Depositar.java
@@ -0,0 +1,59 @@
+package DAO;
+
+import BEAN.Conta;
+import BEAN.Movimentacao;
+import BEAN.Pessoa;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author higor
+ */
+public class Depositar extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+        PrintWriter saida = resp.getWriter();
+        
+        Movimentacao objMovimentacao = new Movimentacao();
+        MovimentacaoDAO objControleMovimentacao = new MovimentacaoDAO();
+        Conta objConta = null;
+        ContaDAO objControleConta = new ContaDAO();
+        
+        int numero = Integer.parseInt(req.getParameter("numero"));
+        float valor = Float.parseFloat(req.getParameter("valor"));
+        
+        try {
+            objConta = objControleConta.consultar(numero);
+        } catch (SQLException ex) {
+            saida.println(ex);
+        }
+        
+        objMovimentacao.setDataLancamento("01-01-2016");
+        objMovimentacao.setDescricao("Deposito");
+        objMovimentacao.setId_conta(objConta.getId());
+        objMovimentacao.setValor(valor);
+        
+        try {
+            objControleMovimentacao.movimentar(objMovimentacao);
+        } catch (SQLException ex) {
+            saida.println(ex);
+        }
+        
+        
+        
+        
+        
+    }
+
+
+}
