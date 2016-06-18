7c09dbb1745c23061b033983a3320c7fb4227d46
GersonEverton <everton.doze@hotmail.com>
2016-06-17 21:49:07 -0300

    teste2

--- BancoMaster/src/java/DAO/Sacar.java
+++ BancoMaster/src/java/DAO/Sacar.java
@@ -17,48 +17,40 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author higor
  */
-public class Depositar extends HttpServlet {
+public class Sacar extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
+
         PrintWriter saida = resp.getWriter();
-        
+
         Movimentacao objMovimentacao = new Movimentacao();
         MovimentacaoDAO objControleMovimentacao = new MovimentacaoDAO();
         Conta objConta = null;
         ContaDAO objControleConta = new ContaDAO();
-        
+
         int numero = Integer.parseInt(req.getParameter("numero"));
         float valor = Float.parseFloat(req.getParameter("valor"));
-        
+
         try {
             objConta = objControleConta.consultar(numero);
         } catch (SQLException ex) {
             saida.println(ex);
         }
-        
         if (objConta.getNumero() == numero) {
             objMovimentacao.setDataLancamento("01-01-2016");
-            objMovimentacao.setDescricao("Deposito");
+            objMovimentacao.setDescricao("Saque");
             objMovimentacao.setId_conta(objConta.getId());
-            objMovimentacao.setValor(valor);
+            objMovimentacao.setValor(valor * -1);
 
             try {
                 objControleMovimentacao.movimentar(objMovimentacao);
-                saida.println("Deposito efetuado");
+                saida.println("Saque efetuado");
             } catch (SQLException ex) {
                 saida.println(ex);
             }
         }else{
             saida.println("Conta não encontrada");
         }
-        
-        
-        
-        
-        
     }
-
-
 }
