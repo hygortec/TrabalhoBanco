b4a3caca1c92150f46ddd39449bcfdf064cc1067
higor <higor@DESKTOP-BF297VI.mshome.net>
2016-06-17 21:09:18 -0300

    Revert "Correções diversas"
    This reverts commit 19bc5dab4a89f00bb3eab13833dce69174a5bb9a.

--- BancoMaster/src/java/DAO/Depositar.java
+++ BancoMaster/src/java/DAO/Depositar.java
@@ -21,38 +21,39 @@ public class Depositar extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        
         PrintWriter saida = resp.getWriter();
-
+        
         Movimentacao objMovimentacao = new Movimentacao();
         MovimentacaoDAO objControleMovimentacao = new MovimentacaoDAO();
         Conta objConta = null;
         ContaDAO objControleConta = new ContaDAO();
-
+        
         int numero = Integer.parseInt(req.getParameter("numero"));
         float valor = Float.parseFloat(req.getParameter("valor"));
-
+        
         try {
             objConta = objControleConta.consultar(numero);
         } catch (SQLException ex) {
             saida.println(ex);
         }
-
-        if (objConta.getNumero() == numero) {
-            objMovimentacao.setDataLancamento("01-01-2016");
-            objMovimentacao.setDescricao("Deposito");
-            objMovimentacao.setId_conta(objConta.getId());
-            objMovimentacao.setValor(valor);
-
-            try {
-                objControleMovimentacao.movimentar(objMovimentacao);
-                saida.println("Depositado");
-            } catch (SQLException ex) {
-                saida.println(ex);
-            }
-
-        } else {
-            saida.println("Conta não encontrada");
+        
+        objMovimentacao.setDataLancamento("01-01-2016");
+        objMovimentacao.setDescricao("Deposito");
+        objMovimentacao.setId_conta(objConta.getId());
+        objMovimentacao.setValor(valor);
+        
+        try {
+            objControleMovimentacao.movimentar(objMovimentacao);
+        } catch (SQLException ex) {
+            saida.println(ex);
         }
+        
+        
+        
+        
+        
     }
+
+
 }
